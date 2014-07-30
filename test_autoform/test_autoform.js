323a128803b95b311a868fdd91761a10a3368b8e
papystiopa <ecolaitis@gmail.com>
2014-07-31 01:06:29 +0200

    and some content to work on

---
+++ test_autoform/test_autoform.js
@@ -0,0 +1,55 @@
+if (Meteor.isClient) {
+  Template.hello.greeting = function () {
+    return "Welcome to test_autoform.";
+  };
+
+  Template.hello.events({
+    'click input': function () {
+      // template data, if any, is available in 'this'
+      if (typeof console !== 'undefined')
+        console.log("You pressed the button");
+    }
+  });
+
+  
+
+
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+    
+    Books = new Meteor.Collection("books", {
+       schema: {
+            title: {
+                type: String,
+                label: "Title",
+                max: 200
+            },
+            author: {
+                type: String,
+                label: "Author"
+            },
+            copies: {
+                type: Number,
+                label: "Number of copies",
+                min: 0
+            },
+            lastCheckedOut: {
+                type: Date,
+                label: "Last date this book was checked out",
+                optional: true
+            },
+            summary: {
+                type: String,
+                label: "Brief summary",
+                optional: true,
+                max: 1000
+            }
+        }
+    });
+
+
+  });
+}
