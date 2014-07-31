0ac211e39ebfd1a6132296f556a3499754ed9b5d
papystiopa <ecolaitis@gmail.com>
2014-07-31 08:32:50 +0200

    test_translation is a success !!

--- test_autoform/test_autoform.js
+++ test_autoform/test_autoform.js
@@ -1,3 +1,35 @@
+Books = new Meteor.Collection("books", {
+   schema: {
+        title: {
+            type: String,
+            label: "Title",
+            max: 200
+        },
+        author: {
+            type: String,
+            label: "Author"
+        },
+        copies: {
+            type: Number,
+            label: "Number of copies",
+            min: 0
+        },
+        lastCheckedOut: {
+            type: Date,
+            label: "Last date this book was checked out",
+            optional: true
+        },
+        summary: {
+            type: String,
+            label: "Brief summary",
+            optional: true,
+            max: 1000
+        }
+    }
+});
+
+
+
 if (Meteor.isClient) {
   Template.hello.greeting = function () {
     return "Welcome to test_autoform.";
@@ -10,6 +42,16 @@ if (Meteor.isClient) {
         console.log("You pressed the button");
     }
   });
+  
+  Template.leaderboard.Books = function () {
+    return Books.find();
+  };
+  
+  
+  
+  Template.updateBookForm.editingDoc = function () {
+    return Books.findOne({_id: Session.get("selectedDocId")});
+  };
 
   
 
@@ -20,35 +62,7 @@ if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
     
-    Books = new Meteor.Collection("books", {
-       schema: {
-            title: {
-                type: String,
-                label: "Title",
-                max: 200
-            },
-            author: {
-                type: String,
-                label: "Author"
-            },
-            copies: {
-                type: Number,
-                label: "Number of copies",
-                min: 0
-            },
-            lastCheckedOut: {
-                type: Date,
-                label: "Last date this book was checked out",
-                optional: true
-            },
-            summary: {
-                type: String,
-                label: "Brief summary",
-                optional: true,
-                max: 1000
-            }
-        }
-    });
+    
 
 
   });
